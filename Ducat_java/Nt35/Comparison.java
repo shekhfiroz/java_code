f9baeebf1330c0866cc14188625dba66d0de6fc4
shekh firoz alam <firozhere@gmail.com>
2016-06-10 23:13:55 +0530

    new programmes_added

---
+++ Ducat_java/Nt35/Comparison.java
@@ -0,0 +1,12 @@
+class ComparisonPool{
+	public static void main(string s[]){
+		integer i1 =new Integer(10);
+		integer i2 =new Integer(10);
+		boolean r =i1==i2;
+		System.out.println(r);
+		Integer i3 =127;
+		Integer i4 = -127;
+		r=i3==i4;
+		System.out.println(r);
+	}
+}
